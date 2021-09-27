50443b9f553828bbedb8d64da176ff9014dea349
seongjin <77876601+ysjkof@users.noreply.github.com>
2021-09-27 23:01:45 +0900

    updateProfile

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -5,7 +5,7 @@ import {
   registerEnumType,
 } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { BeforeInsert, Column, Entity } from 'typeorm';
+import { BeforeInsert, BeforeUpdate, Column, Entity } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { InternalServerErrorException } from '@nestjs/common';
 import { IsEmail, IsEnum } from 'class-validator';
@@ -42,6 +42,8 @@ export class User extends CoreEntity {
   // DB에 저장하기 전에 실행할 기능.
   // user.service.ts에서 .save하기 전에 create를 한다. create는 오브젝트를 만든다. 그래서 이미 password를 가지고 있다.
   @BeforeInsert()
+  // BeforeUpdate는 특정 entity를 update할때 실행된다. editProfile에서 update()는 엔티티를 거치지 않고 DB에 쿼리만 보내기 떄문에 안됨. 그래서 save()로 변경.
+  @BeforeUpdate()
   async hashPassword(): Promise<void> {
     try {
       this.password = await bcrypt.hash(this.password, 10);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,6 +5,7 @@ import { CreateAccountInput } from './dtos/create-account.dto';
 import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 import { JwtService } from 'src/jwt/jwt.service';
+import { EditProfileInput } from './dtos/edit-profile.dto';
 
 @Injectable()
 export class UserService {
@@ -58,4 +59,22 @@ export class UserService {
     // findOne은 TypeORM에서 제공되는 함수.
     return this.users.findOne({ id });
   }
+
+  // {email, password} 이렇게 destructuring syntax 사용하면 둘 중 보내지 않은 값이 있으면 undefined가 전달되서 graphql에러 난다. 해결하려면 아래 update()를 쓸 때처럼 해야한다.
+  // update를 쓰면 entities를 확인하지 않기 때문에 @BeforeUpdate가 실행되지 않는다. 그래서 save를 사용한다.
+  async editProfile(userId: number, { email, password }: EditProfileInput) {
+    const user = await this.users.findOne(userId);
+    if (email) {
+      user.email = email;
+    }
+    if (password) {
+      user.password = password;
+    }
+    return this.users.save(user);
+  }
+  // update()는 db에 실제 존재하는지 체크하지 않고 그냥 실행한다. 빠르다. 로그인한 경우가 아니면 user service에서 editProfile을 실행할 수 없으니까 체크할 필요 없다.
+  // 이 경우에는 userId가 GraphQL에서 오지 않고 token에서 온다. graphql input으로 userid를 받는다면 update를 쓰지 않는다. 왜냐면 아무나 어떤 번호든 보낼 수 있기 떄문.
+  // async editProfile(userId: number, editProfileInput: EditProfileInput) {
+  //   this.users.update(userId, { ...editProfileInput });
+  // }
 }
