30ca05ea839ec2ecf73097dd76ba2c14920d6075
seongjin <77876601+ysjkof@users.noreply.github.com>
2021-09-30 21:17:13 +0900

    delete verification

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -120,8 +120,8 @@ export class UserService {
       );
       if (verification) {
         verification.user.verified = true;
-        console.log(verification.user);
-        this.users.save(verification.user);
+        await this.users.save(verification.user);
+        await this.verifications.delete(verification.id);
         return { ok: true };
       }
       return { ok: false, error: 'Verification not found.' };
