ba8ef07b188cf0c8b0cb70e0c8ecde2d7879b710
seongjin <77876601+ysjkof@users.noreply.github.com>
2021-10-03 18:34:45 +0900

    #7.6 createAccount Test part Two
    테스트는 여기까지만 하고 건너뛴다.

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,25 +2,33 @@ import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
+import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { Verification } from './entities/verification.entity';
 import { UserService } from './users.service';
 
-const mockRepository = {
+const mockRepository = () => ({
   findOne: jest.fn(),
   save: jest.fn(),
   create: jest.fn(),
-};
+});
 const mockJwtService = {
   sign: jest.fn(),
   verify: jest.fn(),
 };
 const mockMailService = {
-  sendVerification: jest.fn(),
+  sendVerificationEmail: jest.fn(),
 };
+
+type MockRepository<T = any> = Partial<
+  Record<keyof Repository<User>, jest.Mock>
+>;
 
 describe('UserService', () => {
   let service: UserService;
+  let usersRepository: MockRepository<User>;
+  let verificationsRepository: MockRepository<Verification>;
+  let mailService: MailService;
 
   beforeAll(async () => {
     // 테스트할 서비스 불러오기.
@@ -28,8 +36,11 @@ describe('UserService', () => {
       //   Mock repository를 불러온다. 가짜 레포지토리로 실제 레포지토리를 불러오지 않는다. 왜냐면 유저 서비스를 단독으로 테스트 하기 위해서.
       providers: [
         UserService,
-        { provide: getRepositoryToken(User), useValue: mockRepository },
-        { provide: getRepositoryToken(Verification), useValue: mockRepository },
+        { provide: getRepositoryToken(User), useValue: mockRepository() },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository(),
+        },
         {
           provide: JwtService,
           useValue: mockJwtService,
@@ -41,13 +52,74 @@ describe('UserService', () => {
       ],
     }).compile();
     service = module.get<UserService>(UserService);
+    mailService = module.get<MailService>(MailService);
+    usersRepository = module.get(getRepositoryToken(User));
+    verificationsRepository = module.get(getRepositoryToken(Verification));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    const createAccountArgs = {
+      email: 'bs@email.com',
+      password: 'bs.password',
+      role: 0,
+    };
+
+    it('should fail if user exists', async () => {
+      // findOne은 DB에 접속해서 값을 반환해야 하는데 unit test를 위해서 db에 연결하지 않았고 그래서 mock으로 대신 설정한 값으로 반환한다.
+      usersRepository.findOne.mockResolvedValue({
+        id: 1,
+        email: 'alalalalalalaa',
+      });
+      const result = await service.createAccount(createAccountArgs);
+      expect(result).toMatchObject({
+        ok: false,
+        error: 'There is a user with that email already',
+      });
+    });
+
+    it('should create a new user', async () => {
+      // await를 사용하는 줄은 promise를 반환하기 때문에 mockResolvedValue, 그게 아닌 경우 값을 반환하는 메서드일 때는 mockReturnValue 사용.
+      usersRepository.findOne.mockResolvedValue(undefined);
+      usersRepository.create.mockReturnValue(createAccountArgs);
+      usersRepository.save.mockResolvedValue(createAccountArgs);
+      verificationsRepository.create.mockReturnValue({
+        user: createAccountArgs,
+      });
+      verificationsRepository.save.mockResolvedValue({
+        code: 'code',
+      });
+
+      const result = await service.createAccount(createAccountArgs);
+
+      expect(usersRepository.create).toHaveBeenCalledTimes(1);
+      expect(usersRepository.create).toHaveBeenCalledWith(createAccountArgs);
+
+      expect(usersRepository.save).toHaveBeenCalledTimes(1);
+      expect(usersRepository.save).toHaveBeenCalledWith(createAccountArgs);
+
+      expect(verificationsRepository.create).toHaveBeenCalledTimes(1);
+      expect(verificationsRepository.create).toHaveBeenCalledWith({
+        user: createAccountArgs,
+      });
+
+      expect(verificationsRepository.save).toHaveBeenCalledTimes(1);
+      expect(verificationsRepository.save).toHaveBeenCalledWith({
+        user: createAccountArgs,
+      });
+
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledTimes(1);
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(
+        expect.any(String),
+        expect.any(String),
+      );
+      expect(result).toEqual({ ok: true });
+    });
+  });
+
   it.todo('login');
   it.todo('findById');
   it.todo('editProfile');
