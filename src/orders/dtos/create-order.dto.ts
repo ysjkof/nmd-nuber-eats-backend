0a8d46e8b8bffc746d2c0d5627c1cf2402ae7dc4
ysjkof <77876601+ysjkof@users.noreply.github.com>
2021-10-22 18:40:06 +0900

    create order part 5

--- src/orders/dtos/create-order.dto.ts
+++ src/orders/dtos/create-order.dto.ts
@@ -1,11 +1,26 @@
 import { Field, InputType, Int, ObjectType, PickType } from '@nestjs/graphql';
 import { CoreOutput } from 'src/common/dtos/output.dto';
-import { Order } from '../entities/order.entity';
+import { DishOption } from 'src/restaurants/entities/dish.entity';
+import { OrderItemOption } from '../entities/order-item.entity';
 
 @InputType()
-export class CreateOrderInput extends PickType(Order, ['items']) {
+class CreateOrderItemInput{
+  @Field(type=>Int)
+  dishId:number;
+
+  @Field(type=>OrderItemOption,{nullable:true})
+  options?:OrderItemOption[];
+}
+
+@InputType()
+// OrderItem 전체를 input하지 않기 위해서. extends하지 않음.
+// export class CreateOrderInput extends PickType(Order, ['items']) {
+export class CreateOrderInput{
   @Field((type) => Int)
   restaurantId: number;
+
+  @Field(type=>[CreateOrderItemInput])
+  items:CreateOrderItemInput[];
 }
 
 @ObjectType()
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Dish } from 'src/restaurants/entities/dish.entity';
+import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { User } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
 
 @Injectable()
@@ -8,5 +13,53 @@ export class OrderService {
   constructor(
     @InjectRepository(Order)
     private readonly orders: Repository<Order>,
+    @InjectRepository(OrderItem)
+    private readonly orderItems: Repository<OrderItem>,
+    @InjectRepository(Restaurant)
+    private readonly restaurants: Repository<Restaurant>,
+    @InjectRepository(Dish)
+    private readonly dishes: Repository<Dish>,
   ) {}
+
+  async createOrder(
+    customer: User,
+    { restaurantId, items }: CreateOrderInput,
+  ): Promise<CreateOrderOutput> {
+    const restaurant = await this.restaurants.findOne(restaurantId);
+    if (!restaurant) {
+      return { ok: false, error: 'Restaurant not found' };
+    }
+    // .forEach에서 return하면 return되지 않는다. 계속 반복문 돌아감. 그래서 for of 사용.
+    for (const item of items) {
+      const dish = await this.dishes.findOne(item.dishId);
+      if (!dish) {
+        return { ok: false, error: 'Dish not found.' };
+      }
+      console.log(`Dish price: ${dish.price}`);
+      for (const itemOption of item.options) {
+        const dishOption = dish.options.find(
+          (dishOption) => dishOption.name === itemOption.name,
+        );
+        if (dishOption) {
+          if (dishOption.extra) {
+            console.log(`$USD + ${dishOption.extra}`);
+          } else {
+            const dishOptionChoice = dishOption.choices.find(
+              (optionChoice) => optionChoice.name === itemOption.choice,
+            );
+            if (dishOptionChoice) {
+              if (dishOptionChoice.extra) {
+                console.log(`$USD + ${dishOptionChoice.extra}`);
+              }
+            }
+          }
+        }
+      }
+      // await this.orderItems.save(this.orderItems.create({dish,options:item.options}))
+    }
+    // const order = await this.orders.save(this.orders.create({
+    //   customer, restaurant
+    // }));
+    // console.log(order)
+  }
 }
