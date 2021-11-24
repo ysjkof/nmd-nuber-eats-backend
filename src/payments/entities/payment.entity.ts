4c7746e3b63973ff4520a59b723dcfd41d260d72
seongjin <77876601+ysjkof@users.noreply.github.com>
2021-11-24 15:52:27 +0900

    create & getPayments

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -8,9 +8,9 @@ import { Column, Entity, ManyToOne, RelationId } from 'typeorm';
 @ObjectType()
 @Entity()
 export class Payment extends CoreEntity {
-  @Field(type => Int)
+  @Field(type => String)
   @Column()
-  transactionId: number;
+  transactionId: string;
 
   @Field(type => User)
   @ManyToOne(type => User, user => user.payments)
@@ -23,6 +23,7 @@ export class Payment extends CoreEntity {
   @ManyToOne(type => Restaurant)
   restaurant: Restaurant;
 
+  @Field(type => Int)
   @RelationId((payment: Payment) => payment.restaurant)
   restaurantId: number;
 }
--- src/payments/payments.resolver.ts
+++ src/payments/payments.resolver.ts
@@ -1,8 +1,32 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { Role } from 'src/auth/role.decorator';
+import { User } from 'src/users/entities/user.entity';
+import {
+  CreatePaymentInput,
+  CreatePaymentOutput,
+} from './dtos/create-payment.dto';
+import { GetPaymentsOutput } from './dtos/get-payments.dto';
 import { Payment } from './entities/payment.entity';
 import { PaymentService } from './payments.service';
 
 @Resolver(of => Payment)
 export class PaymentResolver {
   constructor(private readonly paymentService: PaymentService) {}
+
+  @Mutation(returns => CreatePaymentOutput)
+  @Role(['Owner'])
+  createPayment(
+    @AuthUser() owner: User,
+    @Args('input') createPaymentInput: CreatePaymentInput,
+  ): Promise<CreatePaymentOutput> {
+    return this.paymentService.createPayment(owner, createPaymentInput);
+  }
+
+  //   User entity의 payments에서 { eager: true }하면 아래처럼 안해도 me query에서 transactionId를 가져올 수 있다. 페이지네이션 안됨. 만약 한 유저가 페이먼트가 수천개있다면 페이지네이션 못하고 dager relationship만 요청하면 문제가 생긴다.
+  @Query(returns => GetPaymentsOutput)
+  @Role(['Owner'])
+  getPayments(@AuthUser() user: User): Promise<GetPaymentsOutput> {
+    return this.paymentService.getPayments(user);
+  }
 }
