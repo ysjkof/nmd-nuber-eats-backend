9f47b7a0f642a3f1e4c30b4d7f560bc96f607d1d
seongjin <77876601+ysjkof@users.noreply.github.com>
2021-11-24 16:23:31 +0900

    Task Scheduling with Cron

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,6 +24,7 @@ import { Order } from './orders/entities/order.entity';
 import { OrderItem } from './orders/entities/order-item.entity';
 import { PaymentsModule } from './payments/payments.module';
 import { Payment } from './payments/entities/payment.entity';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
@@ -83,6 +84,7 @@ import { Payment } from './payments/entities/payment.entity';
         };
       },
     }),
+    ScheduleModule.forRoot(),
     JwtModule.forRoot({
       privateKey: process.env.PRIVATE_KEY,
     }),
--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, Interval, SchedulerRegistry, Timeout } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User } from 'src/users/entities/user.entity';
@@ -17,6 +18,7 @@ export class PaymentService {
     private readonly payments: Repository<Payment>,
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
+    private schedulerRegistry: SchedulerRegistry,
   ) {}
 
   async createPayment(
@@ -65,5 +67,27 @@ export class PaymentService {
         error: 'Could not load payments.',
       };
     }
+  }
+
+  //   매 30초마다 실행
+  @Cron('30 * * * * *', {
+    name: 'myJob',
+  })
+  checkForPayments() {
+    console.log('Checking for payments....(cron)');
+    const job = this.schedulerRegistry.getCronJob('myJob');
+    job.stop();
+  }
+
+  //   5초 간격으로 실행
+  @Interval(5000)
+  checkForPaymentsI() {
+    console.log('Checking for payments....(interval)');
+  }
+
+  //   시작하고 20초 뒤에 한번만 실행
+  @Timeout(20000)
+  afterStarts() {
+    console.log('Congrats!');
   }
 }
