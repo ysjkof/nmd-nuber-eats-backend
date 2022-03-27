8126fa541657141a8214ce1d534dc204e347cd7f
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-03-27 15:06:05 +0900

    cors 허용

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   // middleware를 전체경로에서 사용하고 싶다면 이렇게.
   // 이때는 functional middleware만 가능. class 안됨.
   // app.use(jwtMiddleware);
